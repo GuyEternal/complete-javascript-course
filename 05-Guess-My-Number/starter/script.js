a59aa11130687971476947ba803070b233111cc4
Deep Jawale <99432276+GuyEternal@users.noreply.github.com>
2023-05-20 21:53:41 +0000

    Pending changes exported from your codespace

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let secret_num = Math.floor(Math.random() * 20);
+
+// Configure what happens once the button is clicked
+
+// document.querySelector('.message').textContent
+
+document.querySelector('.check').addEventListener('click', function () {
+        let guess = Number(document.querySelector('.guess').value);
+        document.querySelector('.number').textContent = 5;
+        if (guess > secret_num) {
+            document.querySelector('.message').textContent = "Too high";
+        } else if (guess < secret_num) {
+            document.querySelector('.message').textContent = "Too low";
+        }
+        if (guess === secret_num) {
+            document.querySelector('.message').textContent = "You Win!";
+        }
+})
